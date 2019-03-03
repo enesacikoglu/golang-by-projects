ec9eb2a8e23cea41ecda9c4f4343b38adea3f927
Enes Açıkoğlu <enes.acikoglu@trendyol.com>
2019-03-03 20:01:08 +0300

    Commit With LOVE

--- go101/Values.go
+++ go101/Values.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("go" + "lang")
+	fmt.Println("En" + "es")
 
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println("21+40 =", 21+40)
+	fmt.Println("122.0/2.0 =", 122.0/2.0)
 
 	fmt.Println(true && false)
 	fmt.Println(true || false)
---
+++ go101/Variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "I am initial one!"
+	fmt.Println(a)
+
+	var b, c int = 61, 62
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+
+}
